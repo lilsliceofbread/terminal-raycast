c9688bd11253d0de1ecba7d4ab1f368aa5281039
lilsliceofbread <yeshumankar@gmail.com>
2023-07-04 19:11:48 +0800

    nearly working, DDA skipping tiles

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,12 @@
 /*
 TODO:
 make cross-platform using ncurses
+move all into separate functions (e.g. findGridIntersect)
 */
 
 using namespace std::chrono_literals;
 
-#define MAX_WIDTH 70
+#define MAX_WIDTH 120
 #define MAX_HEIGHT 50
 #define FRAME_LENGTH 16.66ms
 
@@ -28,10 +29,11 @@ int main() {
    
     float distances[scrWidth] = {0}; // will store distances from player to surrounds
     std::string screen[scrHeight];
+
     // 1 for wall, 0 for air
     constexpr int mapWidth = 10;
     constexpr int mapHeight = 10;
-    uint8_t map[mapWidth][mapHeight] = {
+    uint8_t map[mapHeight][mapWidth] = {
         {1,1,1,1,1,1,1,1,1,1},
         {1,0,0,0,0,0,0,0,0,1},
         {1,0,0,0,0,0,0,0,0,1},
@@ -48,17 +50,16 @@ int main() {
 
     constexpr float fov = M_PI_4; // 45 deg field of view
     constexpr float turnSpeed = 0.015f;
+    constexpr float playerSpeed = 0.05f;
     // start pos / angle
-    int playerX = 5;
-    int playerY = 5;
+    float playerX = 5;
+    float playerY = 5;
     float playerAngle = 0;
 
     int totalFrames = 0;
+    float planeX, planeY;
     float currAngle;
     float lineEndX, lineEndY;
-    float dxLine,dyLine; // variables for DDA line
-    float step;
-    float currX, currY;
     int squareX, squareY;
     float intersectX, intersectY;
     // game loop
@@ -69,9 +70,17 @@ int main() {
         auto startTime = std::chrono::system_clock::now();
 
         std::system("clear"); // bad fix later
-        std::cout << "\x1b[2J\x1b[H"; 
+        std::cout << "\x1b[2J\x1b[H\x1b[31m"; 
 
         // player movement
+        if(getKeyPressed(XK_w) && !getKeyPressed(XK_s)) {
+            playerX += playerSpeed * cos(playerAngle);
+            playerY += playerSpeed * sin(playerAngle);
+        } else if(getKeyPressed(XK_s) && !getKeyPressed(XK_w)) {
+            playerX -= playerSpeed * cos(playerAngle);
+            playerY -= playerSpeed * sin(playerAngle);
+        }
+
         if(getKeyPressed(XK_Right)) {
             playerAngle += turnSpeed;
         } else if (getKeyPressed(XK_Left)) {
@@ -85,53 +94,39 @@ int main() {
             playerAngle += 2 * M_PI;
         }
 
-        std::cout << "playerAngle: " << playerAngle * (180/M_PI) << " deg\n";
+        std::cout << "angle: " << playerAngle * (180/M_PI) << " x: " << playerX << " y: " << playerY << "\n";
 
         // raycast to calculate distance values 
         for(int i=0; i <= scrWidth; i++) {
             // current angle will go from leftmost of fov to rightmost in steps of fov/scrwidth
             currAngle = (playerAngle - (fov / 2)) + (i * (fov/scrWidth));
 
-            // DDA algorithm to find intersection with block
+            // actual points on screen viewing plane
+            planeX = playerX + (tan(playerAngle - currAngle) * sin(playerAngle)) + cos(playerAngle);
+            planeY = playerY + (tan(playerAngle - currAngle) * cos(playerAngle)) + sin(playerAngle);
 
-            lineEndX = playerX + (rayLength * cos(currAngle));           
-            lineEndY = playerY + (rayLength * sin(currAngle));
+            lineEndX = playerX + rayLength * cos(currAngle);           
+            lineEndY = playerY + rayLength * sin(currAngle);
 
-            // dx = end - start = x + LENcos(angle) - x = LENcos(angle)
-            dxLine = lineEndX - playerX;
-            dyLine = lineEndY - playerY;
+            // find which grid cell it intersects with
+            std::tie(squareX, squareY) = findGridIntersect(playerX, playerY, lineEndX, lineEndY, map, mapHeight, mapWidth);
 
-            step = (abs(dxLine) >= abs(dyLine)) ? abs(dxLine) : abs(dyLine);
+            // then get exact intersect point
+            std::tie(intersectX, intersectY) = raySquareIntersect(
+                                                    playerX, playerY, 
+                                                    lineEndX, lineEndY, 
+                                                    squareX, squareY
+                                                );
 
-            dxLine = dxLine / step;
-            dyLine = dyLine / step;
+            std::cout << "x: " << intersectX << " y: " << intersectY << "\n";
 
-            currX = playerX;
-            currY = playerY;
-            for(int j=1; j <= step; j++) {
-                // may remove
-                squareX = round(currX);
-                squareY = round(currY);
-                // if current square is a wall
-                if(map[squareX][squareY] == 1) {
-                    // distance between wall and player, weird constants to correct for distance
-                    /*distances[i] = 0.8 + (0.4 * sqrt(
-                                        pow((squareX - playerX), 2) + 
-                                        pow((squareY - playerY), 2)));*/
-                    std::tie(intersectX, intersectY) = lineSquareIntersect(
-                                                            playerX, playerY, 
-                                                            lineEndX, lineEndY, 
-                                                            squareX, squareY
-                                                       );
-                    //std::cout << "x: " << intersectX << " y: " << intersectY << "\n";
-                    distances[i] = sqrt(pow((intersectX - playerX), 2) + 
-                                        pow((intersectY - playerY), 2));
-                    break;
-                }
-                currX += dxLine;
-                currY += dyLine;
+            // if no intersection found
+            if(std::isnan(intersectX) || std::isnan(intersectY)) {
+                std::cout << "NaN intersection\n";
             }
-            //std::cout << distances[i] << "\n";
+            // store distance to intersection
+            distances[i] = sqrt(pow((intersectX - planeX), 2) + 
+                                pow((intersectY - planeY), 2));
         }
 
         int currLine = 0.5;
@@ -150,7 +145,7 @@ int main() {
                 halfColumnHeight = scrHeight / (2 * distances[i]);
                 //append character if column is there
                 if(distFromCentre <= halfColumnHeight) {
-                    line.append("#");
+                    line.append("|");
                 } else {
                     line.append(" ");
                 }
@@ -173,5 +168,6 @@ int main() {
         std::this_thread::sleep_for(FRAME_LENGTH - deltaTime); 
     }
     
+    std::cout << "\x1b[0m";
     return 0;
 }
--- src/util.cpp
+++ src/util.cpp
@@ -1,6 +1,7 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 #include <tuple>
+#include <iostream>
 #include <limits>
 #include <cmath>
 #include "util.hpp"
@@ -36,28 +37,33 @@ std::tuple<float, float> lineLineIntersect(float x1, float y1, float x2, float y
     float intersectX, intersectY;
 
     const float denominator = ((x1 - x2) * (y3 - y4)) - ((y1 - y2) * (x3 - x4));
-    if(denominator == 0) { // idk if !denominator would work here, might be slightly faster
+    const float tNumerator = ((x1 - x3) * (y3 - y4)) - ((y1 - y3) * (x3 - x4));
+    const float uNumerator = ((x1 - x3) * (y1 - y2)) - ((y1 - y3) * (x1 - x2));
+
+    const bool isIntersection = (0 <= tNumerator && tNumerator <= denominator) && (0 <= uNumerator && uNumerator <= denominator);
+    if(denominator == 0 || !isIntersection) { // denom == 0 to prevent divide by zero
         const float nan = std::numeric_limits<float>::quiet_NaN();
         return {nan, nan}; // im using NaN to signify no intersection 
     }
 
-    const float xNumerator = (((x1*y2) - (y1*x2)) * (x3 - x4)) - ((x1 - x2) * ((x3*y4) - (y3*x4)));
-    const float yNumerator = (((x1*y2) - (y1*x2)) * (y3 - y4)) - ((y1 - y2) * ((x3*y4) - (y3*x4)));
-    
-    intersectX = xNumerator / denominator;
-    intersectY = yNumerator / denominator;
+    const float uFrac = uNumerator / denominator;
+
+    intersectX = x3 + (uFrac * (x4 - x3));
+    intersectY = y3 + (uFrac * (y4 - y3));
 
     return std::make_tuple(intersectX, intersectY);
 }
 
 // how the fuck did i come up with this
-std::tuple<float, float> lineSquareIntersect(float x1, float y1, float x2, float y2, int squareX, int squareY) {
-    float intersectX, intersectY;
+std::tuple<float, float> raySquareIntersect(float playerX, float playerY, float x2, float y2, int squareX, int squareY) {
+    // must find closest intersection to player as multiple points could intersect
     // square point will be centre, 0.5 dist surrounding it
+
     //const float topLeftX = squareX - 0.5f, topLeftY = squareY - 0.5f;
     //const float topRightX = squareX + 0.5f, topRightY = squareY - 0.5f;
     //const float bottomLeftX = squareX - 0.5f, bottomLeftY = squareY + 0.5f;
     //const float bottomRightX = squareX + 0.5f, bottomRightY = squareY + 0.5f;
+    float intersectX, intersectY;
 
     // instead use array as then we can use a loop which is better
     const float squarePoints[8] =  {        // must organise elements so that we can loop through them
@@ -67,13 +73,15 @@ std::tuple<float, float> lineSquareIntersect(float x1, float y1, float x2, float
         (squareX - 0.5f), (squareY + 0.5f)  // bottom left
     };                                      // then back to top left
 
+    float currIntersectX, currIntersectY;
+    float smallestDist = 1000000000; // some big number for first check to pass
     int index1, index2;
     float edgeX1, edgeY1, edgeX2, edgeY2;
     for(int i=0; i < 4; i++) {
         index1 = 2 * i;
         index2 = index1 + 2;
         // allow to wrap back around for left edge
-        if(index2 >= 8)
+        if(index2 >= 7)
             index2 = 0;
 
         edgeX1 = squarePoints[index1];
@@ -82,12 +90,32 @@ std::tuple<float, float> lineSquareIntersect(float x1, float y1, float x2, float
         edgeY2 = squarePoints[index2+1]; 
 
         // thought i could use structured bindings but apparently not
-        std::tie(intersectX, intersectY) = lineLineIntersect(x1, y1, x2, y2, edgeX1, edgeY1, edgeX2, edgeY2);
+        std::tie(currIntersectX, currIntersectY) = lineLineIntersect(playerX, playerY, x2, y2, edgeX1, edgeY1, edgeX2, edgeY2);
 
-        if(!std::isnan(intersectX) && !std::isnan(intersectY)) // we have found the intersection
-            break;
+        if(!std::isnan(intersectX) && !std::isnan(intersectY)) { // found an intersection
+            // find distance from player
+            const float dist = sqrt(pow(currIntersectX - playerX, 2) + pow(currIntersectY - playerY, 2));
+            if(smallestDist > dist) {
+                intersectX = currIntersectX;
+                intersectY = currIntersectY;
+                smallestDist = dist;
+            }
+        }
     }
+    /*std::tie(intersectX, intersectY) = lineLineIntersect(x1, y1, x2, y2, squarePoints[0], squarePoints[1], squarePoints[2], squarePoints[3]);
+    if(!std::isnan(intersectX) && !std::isnan(intersectY)) {// we have found the intersection
+        return std::make_tuple(intersectX, intersectY); 
+    }
+    std::tie(intersectX, intersectY) = lineLineIntersect(x1, y1, x2, y2, squarePoints[2], squarePoints[3], squarePoints[4], squarePoints[5]);
+    if(!std::isnan(intersectX) && !std::isnan(intersectY)) {// we have found the intersection
+        return std::make_tuple(intersectX, intersectY);
+    }
+    std::tie(intersectX, intersectY) = lineLineIntersect(x1, y1, x2, y2, squarePoints[4], squarePoints[5], squarePoints[6], squarePoints[7]);
+    if(!std::isnan(intersectX) && !std::isnan(intersectY)) {// we have found the intersection
+        return std::make_tuple(intersectX, intersectY);
+    }
+    std::tie(intersectX, intersectY) = lineLineIntersect(x1, y1, x2, y2, squarePoints[6], squarePoints[7], squarePoints[0], squarePoints[1]);*/
 
-    // will return NaN if no intersects found for some reason
+    // will return NaN if no intersects found
     return std::make_tuple(intersectX, intersectY);
 }
